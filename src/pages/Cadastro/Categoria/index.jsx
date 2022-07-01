aabb5eadce3feb876a4e0051eb804e79f2162b43
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-07-01 15:53:09 -0300

    começando implementar formulario

--- src/pages/Cadastro/Categoria/index.jsx
+++ src/pages/Cadastro/Categoria/index.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
+  const [categoria, setCategoria] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('Categoria: ' + categoria);
+    setCategoria('');
+  }
+
   return (
     <>
       <h1>Cadastro de Categorias</h1>
       <Link to="/">Home</Link>
+      <form onSubmit={handleSubmit}>
+        <label>Nome da Categoria</label>
+        <input
+          onChange={(e) => setCategoria(e.target.value)}
+          type="text"
+          value={categoria}
+        />
+        <button>Cadastrar</button>
+      </form>
     </>
   );
 }
