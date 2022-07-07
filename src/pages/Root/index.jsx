c5143bd1e6efbc6793cbc73ac3a617825fd39342
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-07-07 09:24:41 -0300

    ajuste page root

--- src/pages/Root/index.jsx
+++ src/pages/Root/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 import DefaultLayout from '../../components/DefaultLayout';
 import CadastroCategoria from '../Cadastro/Categoria';
 import CadastroVideo from '../Cadastro/Video';
